6a2805c3fa7f222bfee2d3c403658cd1f234d91d
Izzet <izzetates85@gmail.com>
2021-09-11 17:35:18 +0300

    Initial commit Spring Boot project

---
+++ src/test/java/com/izzet/springit/ReditcloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.izzet.springit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReditcloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
